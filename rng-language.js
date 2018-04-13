d829e74a1e2ef422d0cf65efa34f607ad2dbef99
Michael Neas <MichaelNeas@users.noreply.github.com>
2018-04-13 14:35:46 -0400

    Update rng-language.js

--- rng-language.js
+++ rng-language.js
@@ -1,6 +1,19 @@
+var langIndex;
+var links = []
+
+
+fetch("https://en.wikipedia.org/w/api.php?format=json&origin=*&action=parse&page=List_of_programming_languages&prop=links")
+  .then(function(resp) {
+    return resp.json()
+  }).then(function(data) {
+      links = data.parse.links
+  })
+
 var loading = function(e) {
   e.preventDefault();
   e.stopPropagation();
+  langIndex = Math.floor(Math.random() * links.length)
+  console.log(links[langIndex])
   e.target.classList.add('loading');
   e.target.setAttribute('disabled','disabled');
   setTimeout(function(){
@@ -9,14 +22,4 @@ var loading = function(e) {
   },1500);
 };
 
-var btns = document.querySelectorAll('button');
-for (var i=btns.length-1;i>=0;i--) {
-  btns[i].addEventListener('click',loading);
-}
-
-fetch("https://en.wikipedia.org/w/api.php?format=json&origin=*&action=parse&page=List_of_programming_languages&prop=links").then(function(resp) {
-    console.log(resp);
-    return resp.json()
-}).then(function(data) {
-    console.log(data);
-})
+document.querySelectorAll('button')[0].addEventListener('click',loading);
