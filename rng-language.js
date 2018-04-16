22220b92044ba74a25e73decd49d3ad50da0ea24
Michael Neas <MichaelNeas@users.noreply.github.com>
2018-04-15 21:03:16 -0400

    Google search and some styling

--- rng-language.js
+++ rng-language.js
@@ -1,6 +1,6 @@
 var langIndex;
 var links = []
-
+var lang;
 
 fetch("https://en.wikipedia.org/w/api.php?format=json&origin=*&action=parse&page=List_of_programming_languages&prop=links")
   .then(function(resp) {
@@ -12,14 +12,15 @@ fetch("https://en.wikipedia.org/w/api.php?format=json&origin=*&action=parse&page
 var loading = function(e) {
   e.preventDefault();
   e.stopPropagation();
-  langIndex = Math.floor(Math.random() * links.length)
-  console.log(links[langIndex])
+  langIndex = Math.floor(Math.random() * links.length);
+  lang = links[langIndex]['*'];
   e.target.classList.add('loading');
   e.target.setAttribute('disabled','disabled');
   setTimeout(function(){
     e.target.classList.remove('loading');
     e.target.removeAttribute('disabled');
-  },1500);
+    document.getElementById('langy').innerHTML = `<a target="_blank" href=”http://www.google.com/search?q=${lang}”>${lang}</a>`;
+  },1000);
 };
 
 document.querySelectorAll('button')[0].addEventListener('click',loading);
